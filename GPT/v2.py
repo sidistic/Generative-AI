8b6dee5bfc6079c43165d6e08e9bc674473e5483
Siddharth Sagar <ss13973@nyu.edu>
2023-06-07 17:23:07 -0400

    Added Feed Forward for the model to use the attention

--- GPT/v2.py
+++ GPT/v2.py
@@ -102,6 +102,19 @@ class MultiHeadAttention(nn.Module):
         return torch.cat([h(x) for h in self.heads], dim=-1)
 
 
+class FeedForward(nn.Module):
+    """ a simple linear layer followed by a non-linearity """
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, n_embd),
+            nn.ReLU(),
+        )
+    
+    def forward(self, x):
+        return self.net(x)
+
 # Simple bigram model
 class BigramLanguageModel(nn.Module):
 
@@ -111,6 +124,7 @@ class BigramLanguageModel(nn.Module):
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
         self.sa_heads = MultiHeadAttention(4, n_embd//4) # 4 heads of 8-dimensional self-attention
+        self.ffwd = FeedForward(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx,  targets = None):
@@ -121,6 +135,7 @@ class BigramLanguageModel(nn.Module):
         pos_emb = self.position_embedding_table(torch.arange(T, device= device)) #(T,C)
         x = tok_emb + pos_emb # (B, T, C)
         x = self.sa_heads(x)
+        x = self.ffwd(x)
         logits = self.lm_head(x) #(B, T, Vocab_size)
 
         if targets is None:
