ca99b48609ef56be2e262a8b85db4144541b8feb
Siddharth Sagar <ss13973@nyu.edu>
2023-06-07 18:58:19 -0400

    Model Trained and optimized

--- GPT/v2.py
+++ GPT/v2.py
@@ -3,14 +3,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 #hyperparameters
-batch_size = 32
-block_size = 8
-max_iters = 10000
-eval_interval = 300
-learning_rate = 1e-2
+batch_size = 64
+block_size = 256
+max_iters = 5000
+eval_interval = 500
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32
+n_embd = 384
+n_layer = 6
+n_head = 6
+dropout = 0.2
 # ------------
 
 torch.manual_seed(1337)
@@ -74,6 +77,7 @@ class Head(nn.Module):
         self.query = nn.Linear(n_embd, head_size, bias = False)
         self.value = nn.Linear(n_embd, head_size, bias = False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
     
     def forward(self,x):
         B, T, C = x.shape
@@ -84,7 +88,7 @@ class Head(nn.Module):
         wei = q @ k.transpose(-2,-1) * C ** -0.5 #(B, T, C) @ (B, C, T) --> (B, T, T)
         wei = wei.masked_fill(self.tril[:T,:T] == 0, float('-inf')) # (B, T, T)
         wei = F.softmax(wei, dim=-1) #(B, T, T)
-
+        wei = self.dropout(wei)
         #perform the weighted aggregation of the values
         v = self.value(x) #(B, T, C)
         out = wei @ v
@@ -98,10 +102,12 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(n_embd,n_embd)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         out = torch.cat([h(x) for h in self.heads], dim=-1)
         out = self.proj(out)
+        out = self.dropout(out)
         return out
 
 
@@ -111,9 +117,10 @@ class FeedForward(nn.Module):
     def __init__(self, n_embd):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(n_embd, n_embd),
+            nn.Linear(n_embd, 4* n_embd),
             nn.ReLU(),
-            nn.Linear(n_embd, n_embd), # projection layer
+            nn.Linear(4*n_embd, n_embd), # projection layer
+            nn.Dropout(dropout),
         )
     
     def forward(self, x):
@@ -129,10 +136,12 @@ class Block(nn.Module):
         head_size = n_embd//n_head
         self.sa = MultiHeadAttention(n_head, head_size)
         self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd) # normalizes feature across row
+        self.ln2 = nn.LayerNorm(n_embd)
     
     def forward(self, x):
-        x = x + self.sa(x)
-        x = x + self.ffwd(x)
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
         return x
 
 
@@ -144,11 +153,8 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.blocks = nn.Sequential(
-            Block(n_embd, n_head=4),
-            Block(n_embd, n_head=4),
-            Block(n_embd, n_head=4),
-        )
+        self.blocks = nn.Sequential( *[Block(n_embd,n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx,  targets = None):
